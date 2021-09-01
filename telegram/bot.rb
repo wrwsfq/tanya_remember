0e4b99de207ac91fc1096c8b45f1612b1e7b428d
wrwsfq <wrwsfq@gmail.com>
2021-09-01 22:42:49 +0300

    First step

--- telegram/bot.rb
+++ telegram/bot.rb
@@ -7,11 +7,17 @@ token = "1945514473:AAEhK3hrtQez0CuglbtQg40JHzR0KS2KCPk"
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
-    when "/add"
-        bot.api.send_message(chat_id: message.chat.id, text: "Qq")
-    when "/delete"
+    when "Hello"
+        bot.api.send_message(chat_id: message.chat.id, text: "Hello, Tanya. How are u?")
 
-    when "/search"
+    when "Bad"
+        bot.api.send_photo(chat_id: message.chat.id, photo: "AgACAgIAAxkBAAMXYS_OGgIVQ09UPKlfHjr2daeROtMAApi1MRt5_4BJKuD1zPwB5v8BAAMCAAN4AAMgBA") 
+        
+    when "Nice"   
+        bot.api.send_message(chat_id: message.chat.id, text: "😘")
+        
+    when "Image"
+        bot.api.send_photo(chat_id: message.chat.id, photo: "http://lorempixel.com/800/500/")
     end
   end
 end
